0c23daa0706df84902b6bd8c544d490c6c72436e
GY73ghyDE6v <samiu6215.ik@gmail.com>
2019-10-13 15:27:09 +0900

    通常課題まで達成

--- Assets/CarController.cs
+++ Assets/CarController.cs
@@ -1,15 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CoinController : MonoBehaviour
+public class CarController : MonoBehaviour
 {
     //Unityちゃんのオブジェクト
     private GameObject unitychan;
     // Use this for initialization
     void Start()
     {
-        //回転を開始する角度を設定
-        this.transform.Rotate(0, Random.Range(0, 360), 0);
         //Unityちゃんのオブジェクトを取得
         this.unitychan = GameObject.Find("unitychan");
     }
@@ -17,8 +15,6 @@ public class CoinController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //回転
-        this.transform.Rotate(0, 3, 0);
         if (this.transform.position.z < this.unitychan.transform.position.z)
         {
             Destroy(this.gameObject);
